614bddcad850ff014301bad155ab9447dcde1257
arjun98k <arjunkandekar14@gmail.com>
2025-02-23 15:57:07 +0530

    corountines

--- kotcor.kt
+++ kotcor.kt
@@ -1,5 +1,13 @@
+
+import kotlin.concurrent.thread
+
 fun main() {
     println("Main program start ${Thread.currentThread().name}")
     println("Main program End ${Thread.currentThread().name}")
+    thread {
+        println(Thread.currentThread().name)
+        Thread.sleep(1000) //pretend do some work.. may be file upload
+        println("Main work finished: ${Thread.currentThread().name} ")
+    }
 }
 
