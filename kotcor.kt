15a8c82edfed4142df87d1d0b67bb145e5ef9b03
arjun98k <arjunkandekar14@gmail.com>
2025-02-23 16:13:38 +0530

    corountines

--- kotcor.kt
+++ kotcor.kt
@@ -1,15 +1,25 @@
 
-import kotlin.concurrent.thread
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
 // note main thread wait for to finish all background thread work
 fun main() {
     println("Main program start ${Thread.currentThread().name}")
 
-    thread {
+//    thread {
+//        println("fake work started ${Thread.currentThread().name}")
+//        Thread.sleep(1000) //pretend do some work.. may be file upload
+//        println("Main work finished: ${Thread.currentThread().name} ")
+//        // runnung parallel and do not block main thread program
+//    }
+
+   GlobalScope.launch  {
         println("fake work started ${Thread.currentThread().name}")
         Thread.sleep(1000) //pretend do some work.. may be file upload
         println("Main work finished: ${Thread.currentThread().name} ")
         // runnung parallel and do not block main thread program
     }
+    Thread.sleep(1500)
     println("Main program End ${Thread.currentThread().name}")
 }
 
