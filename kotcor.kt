e3f665af13f899e15725c287ddaf2bf9f3e25d89
arjun98k <arjunkandekar14@gmail.com>
2025-02-23 16:03:37 +0530

    corountines

--- kotcor.kt
+++ kotcor.kt
@@ -3,11 +3,13 @@ import kotlin.concurrent.thread
 
 fun main() {
     println("Main program start ${Thread.currentThread().name}")
-    println("Main program End ${Thread.currentThread().name}")
+
     thread {
         println("fake work started ${Thread.currentThread().name}")
         Thread.sleep(1000) //pretend do some work.. may be file upload
         println("Main work finished: ${Thread.currentThread().name} ")
+        // runnung parallel and do not block main thread program
     }
+    println("Main program End ${Thread.currentThread().name}")
 }
 
