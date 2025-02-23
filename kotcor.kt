3989c75cf7fd99be7adc2b9e8a1b2c302b96fa5b
arjun98k <arjunkandekar14@gmail.com>
2025-02-23 16:44:02 +0530

    corountines

--- kotcor.kt
+++ kotcor.kt
@@ -1,6 +1,8 @@
 
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 // note main thread wait for to finish all background thread work
 fun main() {
@@ -15,11 +17,18 @@ fun main() {
 
    GlobalScope.launch  {
         println("fake work started ${Thread.currentThread().name}")
-        Thread.sleep(1000) //pretend do some work.. may be file upload
-        println("Main work finished: ${Thread.currentThread().name} ")
+        delay(2000) // Corountine is supported but thread: T1 is free (not block)
+        println("Main work finished: ${Thread.currentThread().name} ") //Either T1 or some other thread
         // runnung parallel and do not block main thread program
     }
-    Thread.sleep(1500)
+    // blocks the current main thread & wait for coroutine to finish
+    // (practically not right way to wait)
+//    Thread.sleep(3000)
+//    practically useless for real project
+
+    runBlocking {
+        delay(3000) // wait for corountine to finish (practically not a right to wait)
+    }
     println("Main program End ${Thread.currentThread().name}")
 }
 
