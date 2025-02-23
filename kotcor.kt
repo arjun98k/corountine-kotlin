75cc34ab4ae2875ea4c9d89396631c3c66c2e68f
arjun98k <arjunkandekar14@gmail.com>
2025-02-23 15:59:47 +0530

    corountines

--- kotcor.kt
+++ kotcor.kt
@@ -5,7 +5,7 @@ fun main() {
     println("Main program start ${Thread.currentThread().name}")
     println("Main program End ${Thread.currentThread().name}")
     thread {
-        println(Thread.currentThread().name)
+        println("fake work started ${Thread.currentThread().name}")
         Thread.sleep(1000) //pretend do some work.. may be file upload
         println("Main work finished: ${Thread.currentThread().name} ")
     }
