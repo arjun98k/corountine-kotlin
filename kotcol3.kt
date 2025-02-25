cffd783ddc25467d0d2e9109a69009740183dce6
arjun98k <arjunkandekar14@gmail.com>
2025-02-25 20:17:59 +0530

    kotlin corounitne

--- kotcol3.kt
+++ kotcol3.kt
@@ -1,16 +1,16 @@
 import kotlinx.coroutines.*
 
 
-fun main() = runBlocking {
-    println("Main program start ${Thread.currentThread().name}")
+fun main() = runBlocking {      // Create a blocking corountines that executes in current thread
+    println("Main program start ${Thread.currentThread().name}")  //main thread
 
-    val job: Job = launch {
+    val job: Job = launch {  // Thread T1: create a non blocking corountine
         for (i in 1.. 500){
             print("$i.")
             delay(50)
         }
     }
-    delay(200)
+    delay(200)  // Lets print a few values before we cancel
     job.cancelAndJoin()
-    println("\n Main program end: ${Thread.currentThread().name}")
+    println("\n Main program end: ${Thread.currentThread().name}") // main thread
 }
