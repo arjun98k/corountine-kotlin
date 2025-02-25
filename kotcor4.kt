441838ea773fa7f35969bf23fe1cf25d1f089498
arjun98k <arjunkandekar14@gmail.com>
2025-02-26 01:10:16 +0530

    handling CancelllationException

--- kotcor4.kt
+++ kotcor4.kt
@@ -5,20 +5,24 @@ fun main() = runBlocking {      // Create a blocking corountines that executes i
     println("Main program start ${Thread.currentThread().name}")  //main thread
 
     val job: Job = launch(Dispatchers.Default) {  // Thread T1: create a non blocking corountine
-       try {
-        for (i in 1..500) {
+        try {
+            for (i in 1..500) {
 
-            print("$i.")
-           delay(5)
+                print("$i.")
+                delay(5)
 
+            }
+        } catch (ex: CancellationException) {
+            println("\n caught safely exception ${ex.message}")
+        } finally {
+            withContext(NonCancellable) {
+                delay(1000) // genrally we dont use suspending function in finally
+                println("\n close resources in finally ")
+            }
         }
-    } catch (ex: CancellationException){
-    println("\n caught safely exception")
-    }finally {
-        println("\n close resources in finally ")
-    }
     }
     delay(10)  // Lets print a few values before we cancel
-    job.cancelAndJoin()
+    job.cancel(CancellationException("flex my own error"))
+    job.join()
     println("\n Main program end: ${Thread.currentThread().name}") // main thread
 }
