c22fd8856e2d62076a5173323a13c7d7051c8ff2
arjun98k <arjunkandekar14@gmail.com>
2025-02-25 21:52:16 +0530

    yield function in corountine

--- kotcol3.kt
+++ kotcol3.kt
@@ -1,4 +1,5 @@
 import kotlinx.coroutines.*
+import kotlin.concurrent.thread
 
 
 //fun main() = runBlocking {      // Create a blocking corountines that executes in current thread
@@ -15,16 +16,34 @@ import kotlinx.coroutines.*
 //    println("\n Main program end: ${Thread.currentThread().name}") // main thread
 //}
 
+//fun main() = runBlocking {      // Create a blocking corountines that executes in current thread
+//    println("Main program start ${Thread.currentThread().name}")  //main thread
+//
+//    val job: Job = launch {  // Thread T1: create a non blocking corountine
+//        for (i in 1.. 500){
+//
+//            print("$i.")
+//            yield()  // it is faster delay() and or you can use any other suspending function as per you need
+//        }
+//    }
+//    delay(20)  // Lets print a few values before we cancel
+//    job.cancelAndJoin()
+//    println("\n Main program end: ${Thread.currentThread().name}") // main thread
+//}
+
 fun main() = runBlocking {      // Create a blocking corountines that executes in current thread
     println("Main program start ${Thread.currentThread().name}")  //main thread
 
-    val job: Job = launch {  // Thread T1: create a non blocking corountine
-        for (i in 1.. 500){
+    val job: Job = launch(Dispatchers.Default) {  // Thread T1: create a non blocking corountine
+        for (i in 1..500) {
+         if (!isActive){ return@launch  // or you can use break
+                }
             print("$i.")
-            yield()  // it is faster delay() and or you can use any other suspending function as per you need
+            Thread.sleep(1)
+
         }
     }
-    delay(20)  // Lets print a few values before we cancel
+    delay(10)  // Lets print a few values before we cancel
     job.cancelAndJoin()
     println("\n Main program end: ${Thread.currentThread().name}") // main thread
 }
