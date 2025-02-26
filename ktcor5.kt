51014748ad9ec1b81fcf28820279ec5c86f0a089
arjun98k <arjunkandekar14@gmail.com>
2025-02-26 19:00:59 +0530

    without_timeout_corountine_builder

---
+++ ktcor5.kt
@@ -0,0 +1,24 @@
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeout
+
+fun main()= runBlocking { // create a blocking corountine that exexcutes in current Thread (main)
+
+    println("Main program start ${Thread.currentThread().name}")
+
+    withTimeout(1300) {
+        try {
+            for (i in 0..1000) {
+                println("$i.")
+                delay(500)
+            }
+        }catch (ex: TimeoutCancellationException){
+            // code
+        }finally {
+            // code
+        }
+
+    }
+    println("\n Main program ends: ${Thread.currentThread().name}")
+}
