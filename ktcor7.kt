873ac3a302894c806a07c6f58cf50f58180f872d
arjun98k <arjunkandekar14@gmail.com>
2025-02-26 20:13:56 +0530

    coroutine sequential suspending function

--- ktcor7.kt
+++ ktcor7.kt
@@ -3,7 +3,7 @@ import kotlinx.coroutines.runBlocking
 import kotlin.time.measureTime
 
 fun main()= runBlocking {   // Creates a blocking coroutine that executes in current thread (main)
-    println("Main program starts: ${Thread.currentThread().name}")
+    println("Main program starts: ${Thread.currentThread().name}") // main Thread
     val time = measureTime {
   val  msgOne = getMsgone()
   val msgTwo = getMsgTwo()
@@ -11,15 +11,15 @@ fun main()= runBlocking {   // Creates a blocking coroutine that executes in cur
   println("the entire name of $msgOne + $msgTwo")
     }
 println("complete in $time ms")
-    println("Main prgram ends: ${Thread.currentThread().name}")
+    println("Main prgram ends: ${Thread.currentThread().name}")// main thread
 }
 
 suspend fun getMsgone(): String {
-    delay(1000)
+    delay(1000) // pretends to do some work
     return  "Arjun"
 }
 
 suspend fun getMsgTwo(): String {
-    delay(1000)
+    delay(1000)  // pretends to do some work
     return "kandekar"
 }
